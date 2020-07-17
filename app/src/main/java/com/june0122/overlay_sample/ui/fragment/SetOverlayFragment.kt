0d033aa7f472c1b6bafe515c0982087224755822
june0122 <june0122@naver.com>
2020-07-17 18:31:15 +0900

    Overlay Button에서의 Screenshot 기능 구현

--- app/src/main/java/com/june0122/overlay_sample/ui/fragment/SetOverlayFragment.kt
+++ app/src/main/java/com/june0122/overlay_sample/ui/fragment/SetOverlayFragment.kt
@@ -2,27 +2,42 @@ package com.june0122.overlay_sample.ui.fragment
 
 import android.Manifest
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.PixelFormat
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.Image
+import android.media.ImageReader
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.*
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.june0122.overlay_sample.R
 import com.june0122.overlay_sample.ui.service.ForegroundService
+import com.june0122.overlay_sample.ui.service.ForegroundService.Companion.startService
+import com.june0122.overlay_sample.ui.service.ForegroundService.Companion.stopService
 import kotlinx.android.synthetic.main.fragment_set_overlay.*
+import java.nio.ByteBuffer
+
 
 class SetOverlayFragment : Fragment() {
     companion object {
         const val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1
         const val PERMISSIONS_MULTIPLE_REQUEST = 2
+        const val REQUEST_MEDIA_PROJECTION = 1001
     }
 
     private val requiredPermissionList = arrayListOf(
@@ -34,10 +49,33 @@ class SetOverlayFragment : Fragment() {
     private lateinit var mContext: Context
     private lateinit var mActivity: Activity
 
+    private var screenDensity: Int = 0
+    private var displayWidth: Int = 0
+    private var displayHeight: Int = 0
+
+    private var mp: MediaProjection? = null
+    private var mpManager: MediaProjectionManager? = null
+    private var imageReader: ImageReader? = null
+    private var virtualDisplay: VirtualDisplay? = null
+    private var tempDisplayImageView: ImageView? = null
+
+    private var wm: WindowManager? = null
+
+    private lateinit var rootView: View
+    private lateinit var params: WindowManager.LayoutParams
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mActivity = context as Activity
         mContext = context
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        checkOverlayPermission()
+        checkPermissions()
+        startService(mContext, "서비스가 실행 중입니다.")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -47,16 +85,78 @@ class SetOverlayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        tempDisplayImageView = mActivity.findViewById(R.id.tempImageView)
+
+        val displayMetrics = DisplayMetrics()
+        mActivity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        screenDensity = displayMetrics.densityDpi
+        displayWidth = displayMetrics.widthPixels
+        displayHeight = displayMetrics.heightPixels
+
+        mpManager = mActivity.getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+//        if (mpManager != null) {
+//            startActivityForResult(mpManager?.createScreenCaptureIntent(), REQUEST_MEDIA_PROJECTION)
+//        }
+
         activateOverlayButton.setOnClickListener {
             checkOverlayPermission()
             checkPermissions()
-            ForegroundService.startService(view.context, "서비스가 실행 중입니다.")
+            startService(mContext, "서비스가 실행 중입니다.")
         }
 
         deactivateOverlayButton.setOnClickListener {
-            ForegroundService.stopService(view.context)
-            Toast.makeText(context, "서비스가 종료되었습니다.", Toast.LENGTH_SHORT).show()
+            stopService(mContext)
+            Toast.makeText(mContext, "서비스가 종료되었습니다.", Toast.LENGTH_SHORT).show()
         }
+
+        tempScreenshotButton.setOnClickListener {
+//            if (mpManager != null) {
+//                startActivityForResult(mpManager?.createScreenCaptureIntent(), REQUEST_MEDIA_PROJECTION)
+//            }
+
+            if (!Settings.canDrawOverlays(context)) {
+                val intent = Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + context?.packageName)
+                )
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)
+            } else {
+                startActivityForResult(mpManager?.createScreenCaptureIntent(), REQUEST_MEDIA_PROJECTION)
+
+                startService(mContext, "서비스가 실행 중입니다.")
+
+                wm = mContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                params = WindowManager.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        if (Build.VERSION.SDK_INT >= 26) {
+                            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                        } else {
+                            @Suppress("DEPRECATION")
+                            WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+                        },
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        PixelFormat.TRANSLUCENT
+                ).apply {
+                    gravity = Gravity.TOP
+                }
+
+                rootView = LayoutInflater.from(context).inflate(R.layout.fragment_overlay_button, null)
+                wm?.addView(rootView, params)
+
+
+                val screenshotButton: ImageView? = rootView.rootView?.findViewById(R.id.screenshotButton)
+
+                screenshotButton?.setOnClickListener {
+                    getScreenshot()
+                    Toast.makeText(mContext, "Clicked", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
     }
 
     private fun checkOverlayPermission() {
@@ -89,14 +189,66 @@ class SetOverlayFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
-            if (!Settings.canDrawOverlays(mContext)) {
-                Toast.makeText(mContext, "오버레이 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
-            } else {
-                mActivity.startService(Intent(mContext, ForegroundService::class.java))
-                Toast.makeText(context, "서비스가 실행되었습니다.", Toast.LENGTH_SHORT).show()
+        when (requestCode) {
+            ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE -> {
+                if (!Settings.canDrawOverlays(mContext)) {
+                    Toast.makeText(mContext, "오버레이 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+                } else {
+                    mActivity.startService(Intent(mContext, ForegroundService::class.java))
+                    Toast.makeText(context, "서비스가 실행되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            REQUEST_MEDIA_PROJECTION -> {
+                if (resultCode != RESULT_OK) {
+                    Toast.makeText(mContext, "User cancelled", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                if (data != null && resultCode == RESULT_OK) {
+                    mActivity.setResult(RESULT_OK)
+                    Toast.makeText(mContext, "Setup Media Projection", Toast.LENGTH_LONG).show()
+                    setUpMediaProjection(resultCode, data)
+                }
             }
         }
+    }
+
+    private fun setUpMediaProjection(code: Int, intent: Intent) {
+        mp = mpManager?.getMediaProjection(code, intent)
+        setUpVirtualDisplay()
+    }
+
+    private fun setUpVirtualDisplay() {
+        imageReader = ImageReader.newInstance(
+                displayWidth, displayHeight, PixelFormat.RGBA_8888, 2)
+
+        virtualDisplay = mp?.createVirtualDisplay("ScreenCapture",
+                displayWidth, displayHeight, screenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                imageReader?.surface, null, null)
+
+    }
+
+    private fun getScreenshot() {
+        Log.d("debug", "getScreenshot")
+
+        val image: Image = imageReader?.acquireLatestImage() ?: return
+        val planes: Array<Image.Plane> = image.planes
+        val buffer: ByteBuffer = planes[0].buffer
+        val pixelStride: Int = planes[0].pixelStride
+        val rowStride: Int = planes[0].rowStride
+        val rowPadding = rowStride - pixelStride * displayWidth
+
+        val bitmap: Bitmap = Bitmap.createBitmap(
+                displayWidth + rowPadding / pixelStride, displayHeight,
+                Bitmap.Config.ARGB_8888)
+        bitmap.copyPixelsFromBuffer(buffer)
+        image.close()
+
+        tempDisplayImageView?.setImageBitmap(bitmap)
+        Toast.makeText(mContext, "스크린이 캡쳐되었습니다.", Toast.LENGTH_SHORT).show()
+
     }
 
     private fun checkPermissions() {
@@ -124,5 +276,16 @@ class SetOverlayFragment : Fragment() {
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        if (wm != null) {
+            wm?.removeView(rootView)
+        }
+        stopService(mContext)
+
+        Log.d("debug", "release VirtualDisplay")
+        virtualDisplay?.release()
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/june0122/overlay_sample/ui/service/ForegroundService.kt
+++ app/src/main/java/com/june0122/overlay_sample/ui/service/ForegroundService.kt
@@ -8,14 +8,10 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.PixelFormat
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.IBinder
-import android.view.*
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
@@ -24,11 +20,10 @@ import com.june0122.overlay_sample.ui.activity.MainActivity
 
 
 class ForegroundService : Service() {
-    private var xCoordinate: Float = 0f
-    private var yCoordinate: Float = 0f
     private val serviceId = "Foreground Service Example"
-    private var mView: View? = null
-    private var wm: WindowManager? = null
+
+//    protected val context: Context
+//        get() = this
 
     companion object {
         fun startService(context: Context, message: String) {
@@ -85,72 +80,9 @@ class ForegroundService : Service() {
     override fun onCreate() {
         super.onCreate()
 
-        val inflate: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val params = WindowManager.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                if (Build.VERSION.SDK_INT >= 26) {
-                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-                } else {
-                    @Suppress("DEPRECATION")
-                    WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
-                    // TYPE_SYSTEM_OVERLAYはロック画面にもViewを表示できますが、タッチイベントを取得できません
-                },
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                        WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                PixelFormat.TRANSLUCENT
-        )
-        params.gravity = Gravity.TOP
-        mView = inflate.inflate(R.layout.fragment_overlay_button, null)
-
-
-        val screenshotButton: ImageView? = mView?.rootView?.findViewById(R.id.screenshotButton)
-
-        screenshotButton?.setOnClickListener {
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-        }
-
-//        screenshotButton?.setOnTouchListener(object : View.OnTouchListener {
-//            override fun onTouch(v: View, event: MotionEvent?): Boolean {
-//                when (event?.actionMasked) {
-//                    MotionEvent.ACTION_DOWN -> {
-//                        xCoordinate = v.x - event.rawX
-//                        yCoordinate = v.y - event.rawY
-//                        Log.d("XXX", "Down")
-//                        Log.d("XXX", "${params.x} | ${params.y}")
-//                    }
-//                    MotionEvent.ACTION_MOVE -> {
-//                        v.animate()
-//                            .x(event.rawX + xCoordinate)
-//                            .y(event.rawY + yCoordinate)
-//                            .setDuration(0)
-//                            .start()
-//                    }
-//                    MotionEvent.ACTION_UP -> {
-//                    }
-//                    else -> return false
-//                }
-//                return true
-//            }
-//        })
-
-//        params.x = ViewGroup.LayoutParams.WRAP_CONTENT
-//        params.y = ViewGroup.LayoutParams.WRAP_CONTENT
-//        wm?.updateViewLayout(mView, params)
-
-        wm?.addView(mView, params)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
-        if (wm != null) {
-            if (mView != null) {
-                wm?.removeView(mView)
-                mView = null
-            }
-        }
     }
 }
