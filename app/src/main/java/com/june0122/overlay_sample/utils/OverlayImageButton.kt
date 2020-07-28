5b25144dbe0fa929b3fbb3f22581980b6dca8a87
june0122 <june0122@naver.com>
2020-07-28 17:07:42 +0900

    refactor : SetOverlayFragment Class 내부에 버튼 기능 정의 (#13)

--- app/src/main/java/com/june0122/overlay_sample/utils/OverlayImageButton.kt
+++ app/src/main/java/com/june0122/overlay_sample/utils/OverlayImageButton.kt
@@ -2,47 +2,27 @@ package com.june0122.overlay_sample.utils
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatImageButton
-import java.util.*
-
 
 class OverlayImageButton: AppCompatImageButton {
+    constructor(context: Context) : super(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     companion object {
         const val MAX_CLICK_DURATION = 200
     }
 
-    private var startClickTime : Long = 0
-    private var xCoordinate: Float = 0f
-    private var yCoordinate: Float = 0f
-    constructor(context: Context) : super(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
-
     override fun onTouchEvent(event: MotionEvent): Boolean {
         super.onTouchEvent(event)
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                startClickTime = Calendar.getInstance().timeInMillis
-
-                xCoordinate = rootView.x - event.rawX
-                yCoordinate = rootView.y - event.rawY
-                return true
             }
             MotionEvent.ACTION_MOVE -> {
-                rootView.animate()
-                    .x(event.rawX + xCoordinate)
-                    .y(event.rawY + yCoordinate)
-                    .setDuration(0)
-                    .start()
             }
             MotionEvent.ACTION_UP -> {
-               val clickDuration = Calendar.getInstance().timeInMillis - startClickTime
-                if(clickDuration < MAX_CLICK_DURATION) {
-                    performClick()
-                }
+                performClick()
             }
         }
         return false
@@ -52,12 +32,7 @@ class OverlayImageButton: AppCompatImageButton {
     // normal touch events and for when the system calls this using Accessibility
     override fun performClick(): Boolean {
         super.performClick()
-        Log.d("debug", "move")
-        doSomething()
+        // do something
         return true
-    }
-
-    private fun doSomething() {
-//        Toast.makeText(context, "did something", Toast.LENGTH_SHORT).show()
     }
 }
