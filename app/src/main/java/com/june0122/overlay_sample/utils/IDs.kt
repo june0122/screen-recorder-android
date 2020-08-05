539fafbb66c9cc2619c4bbe866437d94d6cc2a4e
june0122 <june0122@naver.com>
2020-08-05 16:00:13 +0900

    refactor : Request Code나 Service ID 등을 별개의 클래스에 정의 (#18)

--- app/src/main/java/com/june0122/overlay_sample/utils/IDs.kt
+++ app/src/main/java/com/june0122/overlay_sample/utils/IDs.kt
@@ -1,2 +1,9 @@
 package com.june0122.overlay_sample.utils
 
+const val SCREENSHOT_SERVICE_ID = 10
+const val SCREEN_RECORD_SERVICE_ID = 11
+
+// Pending Intent Request Code
+const val PI_CODE_DELETE_SCREENSHOT_SERVICE = 1
+const val PI_CODE_DELETE_SCREEN_RECORD_SERVICE = 2
+const val PI_CODE_STOP_SCREEN_RECORD_SERVICE = 3
