c1f1cef5266560b722eeee0d74e1eefd48fa2bcd
june0122 <june0122@naver.com>
2020-08-07 17:37:38 +0900

    fix : DisplayMetrics 대신 Display 클래스를 통하여 display size
    Display size 값을 얻기 위해 DisplayMetrics 클래스의 widthPixels(), heightPixels()를 사용할 경우
    mediaRecorder의 setVideoSize()의 파라미터에 따라 physical device와 virtual device 중 한쪽에서
    에러가 발생하는 문제점이 있어 수정을 하였으나 virtual device display의 특정 영역에서
    overlay button이 보이지않는 버그가 있었는데 Display 클래스의 getDisplay(), getRealSize() 를
    사용함으로써 버그도 해결되고 코드가 간결하게 처리가 되었음.
    
    Resolves: #22

--- app/src/main/java/com/june0122/overlay_sample/utils/Utils.kt
+++ app/src/main/java/com/june0122/overlay_sample/utils/Utils.kt
@@ -1,6 +1,13 @@
+@file:Suppress("unused")
+
 package com.june0122.overlay_sample.utils
 
+import android.content.Context
+import android.graphics.Point
+import android.hardware.display.DisplayManager
 import android.util.DisplayMetrics
+import android.view.Display
+import androidx.core.content.ContextCompat
 
 //@Suppress("SameParameterValue")
 fun clipInt(min: Int, max: Int, v: Int) = when {
@@ -14,3 +21,12 @@ fun Float.dp2px(dm: DisplayMetrics) =
 
 fun Int.px2dp(dm: DisplayMetrics) =
         this.toFloat() / dm.density
+
+inline fun <reified T> systemService(context: Context): T? =
+        ContextCompat.getSystemService(context, T::class.java)
+
+fun getScreenSize(context: Context) = Point().also {
+    systemService<DisplayManager>(context)
+            ?.getDisplay(Display.DEFAULT_DISPLAY)
+            ?.getRealSize(it)
+}
